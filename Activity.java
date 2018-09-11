dee8d1c4814572acb1929f8331fd9cf1c137a94d
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-09-11 23:40:39 +0530

    Create Activity.java

---
+++ Activity.java
@@ -0,0 +1,40 @@
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Bundle ;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.FileInputStream;
+import java.FileOutputStream;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class Activity {
+    private static final String HIGH_SCORE = "high score";
+    private static final String COIN_SIDE = "coin side";
+    private static final String SCORE = "score";
+    private static final String HIST = "hist";
+    
+    
+ private ImageView coinImage;
+ private TextView scoreText;
+ private TextView highScoreText;
+ private TextView hist;
+ private Button headsButton;
+ private Button tailsButton;
+ 
+ // in progress/
+    
+    
+}
+
+
+
+
+
